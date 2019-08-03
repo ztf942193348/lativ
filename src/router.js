37c3baac05bff874c8b29fa8edc73150d6f8c0bb
ztf <942193348@qq.com>
2019-08-03 18:09:34 +0800

    修改router.js

--- src/router.js
+++ src/router.js
@@ -5,39 +5,59 @@ Vue.use(Router)
 import Home from './pages/Home.vue'
 export default new Router({
   routes: [
-    //重定向路由
-    { path: '/', redirect: { name: 'main' }},
+    //重定向
+    { path: '/', redirect: { name: 'main' } },
+    //一层路由
     {
       path: '/',
       name: 'home',
       component: Home,
-      children:[
+      //嵌套路由(二层)
+      children: [
         {
-          path:'main',
-          name:'main',
+          path: 'main',
+          name: 'main',
           component: Home.components.Zhome
-        },{
-          path:'women',
-          name:'women',
+        }, {
+          path: 'women',
+          name: 'women',
           component: Home.components.Zwomen
-        },{
-          path:'man',
-          name:'man',
+        }, {
+          path: 'man',
+          name: 'man',
           component: Home.components.Zman
-        },{
-          path:'kids',
-          name:'kids',
+        }, {
+          path: 'kids',
+          name: 'kids',
           component: Home.components.Zkids
-        },{
-          path:'baby',
-          name:'baby',
+        }, {
+          path: 'baby',
+          name: 'baby',
           component: Home.components.Zbaby
-        },{
-          path:'sports',
-          name:'sports',
+        }, {
+          path: 'sports',
+          name: 'sports',
           component: Home.components.Zsports
+        },{
+          //这里开始是下面的
+          path: 'fenlei',
+          name: 'fenlei',
+          component: fenlei,
+        },{
+          path: 'jingxuan',
+          name: 'jingxuan',
+          component: jingxuan,
+        },{
+          path: 'gouwuche',
+          name: 'gouwuche',
+          component: gouwuche,
+        },{
+          path: 'geren',
+          name: 'geren',
+          component: geren,
         }
       ]
     }
+
   ]
 })
